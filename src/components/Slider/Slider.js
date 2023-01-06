552e2cce2f39784edf66b7423336a57ad4f0b7da
vandanakaviti <kavitivandana@gmail.com>
2023-01-06 16:26:08 +0530

    Recorrected a minor change I made

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -50,7 +50,7 @@ export default function Slider() {
         setSlides(res.data);
     })
     
-    const delay = 1000;
+    const delay = 4000;
     const timeoutRef = React.useRef(null);
 
     React.useEffect(() => {
